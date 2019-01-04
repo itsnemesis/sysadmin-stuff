bcce598bedbe9c23e6a3e6b6fd68b7e4fd716bbd
Luthernox <46387037+luthernox@users.noreply.github.com>
2019-01-04 18:17:58 -0500

    Update Get-ComputerSystemInfo.ps1

--- Get-ComputerSystemInfo.ps1
+++ Get-ComputerSystemInfo.ps1
@@ -1,95 +1,94 @@
-Function Get-ComputerSystemInfo {
-
-    Param
-	(
-		[Alias('Computer','ComputerName','HostName')]
-		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true,Position=1)]
-		[string[]]$Name = $env:COMPUTERNAME
-	)
-
-    $Results = @()
-    $Count = $name.Count
-    $i = 1
-    
-    foreach ($computer in $name){
-        Write-host "Starting $computer ($i of $count)"
-
-        $computerInfo = [pscustomobject]@{
-                            Computername = $Computer.ToUpper()
-                            Make = $NULL
-                            Model = $NULL
-                            SN = $NULL
-                            IP = $NULL
-                            MAC = $NULL
-                            LastLogonUser = $NULL
-                            Note = $NULL
-        }
-
-        Write-Host "Pinging host"
-        If((test-connection -ComputerName $computer -Count 1 -Quiet)){
-
-            Write-host "Getting Make and Model"
-            $win32computersystem = get-wmiobject win32_computersystem -ComputerName $computer -ErrorAction SilentlyContinue
-            if(($win32computersystem)){
-                $computerInfo.Computername = $win32computersystem.Computername.ToUpper()
-                $computerInfo.Make = $win32computersystem.Manufacturer
-                $computerInfo.Model = $win32computersystem.Model
-            }else{Write-Host -ForegroundColor Red "Unable to retreive win32_Computersystem"}
-
-            ############################################################################################
-            Write-Host "Getting Serial Number"
-            $win32BIOS = get-wmiobject win32_BIOS -ComputerName $computer -ErrorAction SilentlyContinue
-            if(($win32BIOS)){
-                $computerInfo.SN = $win32BIOS.SerialNumber
-            }else{Write-Host -ForegroundColor Red "Unable to retreive win32_BIOS"}
-
-            ############################################################################################
-            Write-Host "Getting IP and MAC Info"
-            $win32NetAdaptConfig = get-wmiobject Win32_NetworkAdapterConfiguration -ComputerName $computer | Select IPAddress, MACAddress | Where IPaddress -GT 1 -ErrorAction SilentlyContinue
-            if(($win32BIOS)){
-                $computerInfo.IP = $win32NetAdaptConfig.IPAddress
-                $ComputerInfo.MAC = $win32NetAdaptConfig.MACAddress
-            }else{Write-Host -ForegroundColor Red "Unable to retreive win32_BIOS"}
-
-            ############################################################################################
-            Write-Host "Getting Last 3 Loged on Users"
-            $LastLogonUser = get-wmiobject Win32_UserProfile -ComputerName $computer -Filter "NOT SID = 'S-1-5-18' AND NOT SID = 'S-1-5-19' AND NOT SID = 'S-1-5-20'" | sort lastUseTime -Descending | select -First 3
-            
-            if(($LastLogonUser)){
-
-                $users = @()
-                
-                Foreach ( $u in $LastLogonUser){
-
-                    $User = [pscustomobject] @{
-                                User = $NULL
-                                Date = $NULL
-                            }
-                    $Script:UserSID = New-Object System.Security.Principal.SecurityIdentifier($u.SID) 
-
-                    $user.User = $Script:UserSID.Translate([System.Security.Principal.NTAccount]) 
-                    $user.Date = ([WMI]'').ConvertToDateTime($u.LastUseTime)
-
-                    $Users += $user
-                }
-                $computerInfo.LastLogonUser = $users
-                
-            }else{Write-Host -ForegroundColor Red "Unable to retreive Win32_UserProfile"}
-
-            ############################################################################################
-
-        }else{
-            Write-Host -ForegroundColor red "$computer Offline"
-            $computerInfo.Note = "Offline"    
-        }
-        #increase counter
-        $i++
-
-        $Results += $computerInfo
-
-
-    }
-
-    $Results
-
+Function Get-FolderBrowserDialog{
+    $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $FolderBrowser.ShowDialog() | Out-Null
+    Return $FolderBrowser.SelectedPath
 }
+
+Function Save-FileName($initialDirectory)
+{   
+ [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+ $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+ $OpenFileDialog.initialDirectory = $initialDirectory
+ $OpenFileDialog.filter = “CSV files (*.csv)|*.csv|All files (*.*)|*.*”
+ $OpenFileDialog.ShowDialog() | Out-Null
+ $OpenFileDialog.filename
+} #end function Get-FileName
+
+#Variable to hold folder to recurse from
+$RootFolder = $null
+
+#User input for FolderBrowser Select or command line input
+do{
+    #User Action to Remove. Yes or No
+    Write-Host -Foreground Yellow "`n`nHow do you want to enter input:`n  1) Using Folder Browser Dialog Form.`n  2) Using command line input."
+    [string]$answer = Read-Host "`n`nEnter 1 or 2..."
+    
+    switch($answer) {
+        #If the user chooses FolderDialogBrowser Form
+        {($answer -eq "1")} { 
+            #Opens FodlerDialogBrowser
+            Write-Host -ForegroundColor Yellow "`n`nSelect a folder to recurse from...`nFolder may be hidden by front window."
+            Read-host "Hit Enter to continue..." | Out-Null
+            #Opens FolderBroswerDialog to select Root Folder
+            $RootFolder = Get-FolderBrowserDialog
+        }
+        #If the user Chooses commmand line input 
+        {($answer -eq "2")} {
+            Write-Host -ForegroundColor Yellow "`nEnter folder recurse from."
+            $RootFolder = Read-Host  "Path--->"
+
+        }
+        #input error
+        default { Write-Host -ForegroundColor Red "You did not enter 1 or 2. No action will be taken.`n" } 
+                    }
+}until($answer -eq "1"-or $answer -eq "2")
+
+
+
+#Script Pause
+Write-Host -ForegroundColor Yellow "`n`nSelect *.csv for output..."
+Read-host " Hit Enter to continue..." | Out-Null
+
+#Opens Open File Dialog to select save file
+$SaveFile = Save-FileName -initialDirectory "%USERDATA%\desktop\"
+
+#Setting up csv and saving to disk
+Write-Host -ForegroundColor cyan "Setting up $SaveFile to disk"
+$csv = "Path,Owner,Group,Access`r`n"
+$fso = new-object -comobject scripting.filesystemobject
+$file = $fso.CreateTextFile($SaveFile,$true)
+$file.write($csv)
+$File.close()
+
+#Gets SubFolders
+Write-Host -ForegroundColor Cyan "Getting Subfolders of $RootFolder"
+$SubFolders = Get-ChildItem $RootFolder -Recurse | ? {$_.psIscontainer -eq $true} | select fullname -ExpandProperty fullname
+
+#Gets ACL information for each subfolder
+Write-Host -ForegroundColor Cyan "Getting Subfolders ACL Information and Writing to disk..."
+Foreach ($folder in $SubFolders){
+   #Gets ACL info 
+   $p = Get-Acl "$folder" | Select Path, Owner,Group,Access
+   #formatting
+   $path = $p.Path.Trim("Microsoft.PowerShell.Core\FileSystem")
+   $owner = $p.Owner
+   $group = $p.Group
+   $access = $p.Access | Select IdentityReference,AccessControlType,FileSystemRights
+   #more formatting for Access object
+   $access_string =$null
+   Foreach($a in $access){
+        $IdentityReference = $a.IdentityReference 
+        $AccessControlType = $a.AccessControlType
+        $FileSystemRights = $a.FileSystemRights
+        $access_string += "$IdentityReference $AccessControlType $FileSystemRights||"
+   }
+
+   #writing output
+   $Output_String = "$path,$owner,$group,$access_string"
+   $Output_String | Out-File $SaveFile -Append
+   
+}
+#Complete
+Write-Host -ForegroundColor Green "Complete. File saved at $savefile"
+
