b64d4995f0c9120b2dad7c80dd8165b0465f304b
Luthernox <46387037+luthernox@users.noreply.github.com>
2019-01-04 18:15:43 -0500

    Create Get-InstalledSoftware.ps1

---
+++ Get-InstalledSoftware.ps1
@@ -0,0 +1,79 @@
+Function Get-InstalledSoftware
+{
+	Param
+	(
+		[Alias('Computer','ComputerName','HostName')]
+		[Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true,Position=1)]
+		[string[]]$Name = $env:COMPUTERNAME
+	)
+	Begin
+	{
+		$LMkeys = "Software\Microsoft\Windows\CurrentVersion\Uninstall","SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+		$LMtype = [Microsoft.Win32.RegistryHive]::LocalMachine
+		$CUkeys = "Software\Microsoft\Windows\CurrentVersion\Uninstall"
+		$CUtype = [Microsoft.Win32.RegistryHive]::CurrentUser
+		
+	}
+	Process
+	{
+		ForEach($Computer in $Name)
+		{
+			$MasterKeys = @()
+			If(!(Test-Connection -ComputerName $Computer -count 1 -quiet))
+			{
+				Write-Error -Message "Unable to contact $Computer. Please verify its network connectivity and try again." -Category ObjectNotFound -TargetObject $Computer
+				Break
+			}
+			$CURegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($CUtype,$computer)
+			$LMRegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($LMtype,$computer)
+			ForEach($Key in $LMkeys)
+			{
+				$RegKey = $LMRegKey.OpenSubkey($key)
+				If($RegKey -ne $null)
+				{
+					ForEach($subName in $RegKey.getsubkeynames())
+					{
+						foreach($sub in $RegKey.opensubkey($subName))
+						{
+							$MasterKeys += (New-Object PSObject -Property @{
+							"ComputerName" = $Computer
+							"Name" = $sub.getvalue("displayname")
+							"SystemComponent" = $sub.getvalue("systemcomponent")
+							"ParentKeyName" = $sub.getvalue("parentkeyname")
+							"Version" = $sub.getvalue("DisplayVersion")
+							"UninstallCommand" = $sub.getvalue("UninstallString")
+							})
+						}
+					}
+				}
+			}
+			ForEach($Key in $CUKeys)
+			{
+				$RegKey = $CURegKey.OpenSubkey($Key)
+				If($RegKey -ne $null)
+				{
+					ForEach($subName in $RegKey.getsubkeynames())
+					{
+						foreach($sub in $RegKey.opensubkey($subName))
+						{
+							$MasterKeys += (New-Object PSObject -Property @{
+							"ComputerName" = $Computer
+							"Name" = $sub.getvalue("displayname")
+							"SystemComponent" = $sub.getvalue("systemcomponent")
+							"ParentKeyName" = $sub.getvalue("parentkeyname")
+							"Version" = $sub.getvalue("DisplayVersion")
+							"UninstallCommand" = $sub.getvalue("UninstallString")
+							})
+						}
+					}
+				}
+			}
+			$MasterKeys = ($MasterKeys | Where {$_.Name -ne $Null -AND $_.SystemComponent -ne "1" -AND $_.ParentKeyName -eq $Null} | select Name,Version,ComputerName,UninstallCommand | sort Name)
+			$MasterKeys
+		}
+	}
+	End
+	{
+		
+	}
+}
